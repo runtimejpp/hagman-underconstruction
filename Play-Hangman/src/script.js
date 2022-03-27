d622259fef182f28aacdba4c666d8d7b7527e39d
Jay Pren <37848207+runtimejpp@users.noreply.github.com>
2022-03-27 01:42:21 -0400

    Add files via upload

---
+++ Play-Hangman/src/script.js
@@ -0,0 +1,16 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+
+const words = ['application', 'programming', 'interface' , ' wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+console.log(selectedWord);
+
